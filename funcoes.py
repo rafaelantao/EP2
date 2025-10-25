06900f14fb403c2894ce75985bc52668eef3368e
TeoLacerda <laacerdateo@gmail.com>
2025-10-24 22:12:11 -0300

    Questao1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,11 @@
-#teste
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicao = []
+    while tamanho > 0:
+        posicao.append([linha, coluna])
+        tamanho -= 1
+
+        if orientacao == 'vertical':
+            linha += 1
+        if orientacao == 'horizontal':
+            coluna += 1
+    return posicao
